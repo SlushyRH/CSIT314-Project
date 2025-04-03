392d75c7c098cac87eef882c3dce347721b8f118
Lachlan Henderson <lrhenderson04@gmail.com>
2025-04-03 18:39:49 +1100

    Added Header and Footer
    - Added header and footer HTML.
    - Added functionality to main.js so it auto loads header and footer into every page using main.js.
    - Added variables to css.

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// load header and footer into each page
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => document.getElementById("header").innerHTML = data);
+
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => document.getElementById("footer").innerHTML = data);
+});
