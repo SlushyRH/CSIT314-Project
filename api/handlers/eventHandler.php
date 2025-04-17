a2eb3b85585e1c313d14c8e61f119c05c5dff3d3
Lachlan Henderson <lrhenderson04@gmail.com>
2025-04-17 13:23:28 +1000

    Update eventHandler.php

---
+++ api/handlers/eventHandler.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once __DIR__ '/../helpers.php';
+
+function getAllEvents()
+{
+    $pdo = getPDO();
+
+    try
+    {
+        $stmt = $pdo->prepare("
+            SELECT 
+                e.event_id,
+                e.organiser_id,
+                e.title,
+                e.description,
+                e.location,
+                e.event_date,
+                ec.name AS category
+            FROM Events e
+            LEFT JOIN EventCategories ec ON e.category_id = ec.category_id
+            ORDER BY e.event_date ASC
+        ");
+
+        $stmt->execute();
+        $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        send_response('success', 'Events fetched successfully.', 200, $events);
+    }
+    catch (Exception $e)
+    {
+        send_response('error', 'Could not fetch events. Error: ' . $e->getMessage(), 500);
+    }
+}
+
+
+?>
