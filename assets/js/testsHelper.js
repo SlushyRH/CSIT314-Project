fac0693fc651f6736477b2c97d1b39412ecc24cc
Lachlan Henderson <lrhenderson04@gmail.com>
2025-05-16 18:53:18 +1000

    Updated test

--- assets/js/testsHelper.js
+++ assets/js/testsHelper.js
@@ -8,6 +8,10 @@ function resetTestOutcome() {
     testOutcome.innerText = '';
 }
 
+function log(msg) {
+    testOutcome.innerText += '\n' + msg;
+}
+
 function assert(condition, msg) {
     const result = condition ? `PASS: ${msg}` : `FAIL: ${msg}`;
     testOutcome.innerText +=  '\n' + result;
