d337883352e6f7a452c7ab6fdc39a1d32496c47f
Lachlan Henderson <lrhenderson04@gmail.com>
2025-05-23 16:45:55 +1000

    Update database.php

--- api/database.php
+++ api/database.php
@@ -553,6 +553,7 @@ function getBookedEvents($pdo, $data)
 
             $ticketStmt = $pdo->prepare("
                 SELECT
+                    tt.ticket_type_id
                     tt.name AS ticket_type,
                     tt.price,
                     rt.quantity
