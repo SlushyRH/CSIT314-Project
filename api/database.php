8e247eff2a8abd5cfe128a884af956bca89e0011
Lachlan <lrhenderson04@gmail.com>
2025-05-28 02:33:09 +1000

    Update database.php

--- api/database.php
+++ api/database.php
@@ -944,10 +944,10 @@ function getUserNotifications($pdo, $data)
 
 try {
     // gets database details from server
-    $db_host = getenv('DB_HOST');
-    $db_name = getenv('DB_NAME');
-    $db_user = getenv('DB_USER');
-    $db_pass = getenv('DB_PASS');
+    $db_host = $_SERVER('DB_HOST');
+    $db_name = $_SERVER('DB_NAME');
+    $db_user = $_SERVER('DB_USER');
+    $db_pass = $_SERVER('DB_PASS');
 
     // establish connection to sql database
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass, [
